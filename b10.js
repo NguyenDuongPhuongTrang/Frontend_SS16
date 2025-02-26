a7aa75b4b1b46b559bc259ab093f373d19851fd0
Changxinhh <nggtrang26@gmail.com>
2025-02-26 10:29:54 +0700

    js

--- b10.js
+++ b10.js
@@ -1,10 +1,3 @@
-let string = prompt("Mời nhập chuỗi");
-let newString = string.trim();
-alert(newString);
-let count = 1;
-for (let i = 0; i < newString.length; i++) {
-    if(newString[i] !== " " && newString[i+1]==" "){
-        count++;
-    }
-}
-console.log(`Số lượng từ trong chuỗi ${string} là: ${count}`);
+let string = prompt("Mời nhập chuỗi").trim();
+let newString = string.split(" ");
+alert(newString.length);
